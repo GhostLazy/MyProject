8397a65c9089c212bdb8caaca839902a71718fd0
GhostLazy <2265041410@qq.com>
2026-01-21 16:20:03 +0800

    22-6
    经验奖励标准设置流程

--- Private/Character/AuraCharacter.cpp
+++ Private/Character/AuraCharacter.cpp
@@ -18,6 +18,8 @@ AAuraCharacter::AAuraCharacter()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
+	
+	CharacterClass = ECharacterClass::Elementalist;
 }
 
 /* Server Initialization */
