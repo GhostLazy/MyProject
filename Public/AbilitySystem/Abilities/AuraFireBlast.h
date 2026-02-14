1c40adc4e91dbe8fa1288d764b9fa14ba7080550
GhostLazy <2265041410@qq.com>
2026-02-14 12:42:00 +0800

    29-1
    火爆术初始化

--- Public/AbilitySystem/Abilities/AuraFireBlast.h
+++ Public/AbilitySystem/Abilities/AuraFireBlast.h
@@ -14,4 +14,14 @@ class MYPROJECT_API UAuraFireBlast : public UAuraDamageGameplayAbility
 {
 	GENERATED_BODY()
 	
+public:
+	
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "FireBlast")
+	int32 NumFireBalls = 12;
+	
 };
